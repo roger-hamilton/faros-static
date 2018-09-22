9f8f664994cb0cee5800a37ecb0604b947e5bfc7
Roger Hamilton <roger.max.hamilton@gmail.com>
2018-09-21 23:01:02 -0500

    Initial Commit

---
+++ src/components/Bio.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+export default class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Roger Hamilton`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <p>
+          Written by <strong>Roger Hamilton</strong> who lives and works in Oklahoma City
+          building useful(-ish) things.{' '}
+          <a href="https://twitter.com/jollyroger726">
+            You should follow him on Twitter
+          </a>
+        </p>
+      </div>
+    )
+  }
+}
---
+++ src/pages/go-in-js/index.md
@@ -0,0 +1,5 @@
+---
+title: Running Go code in JavaScript
+date: "2018-09-21"
+---
+First Lines...
